8d19b2a7aae3dad54d3e1e12fe953caabbb8add5
Glyn L <glyn_92@hotmail.com>
2019-01-12 12:18:56 +0700

    basic fetching/display

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,56 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Form from "./Form";
 import { API_KEY } from "../variables";
 
 // top coins https://coinmarketcap.com/all/views/all/
 
+const Coins = ({ coins }) => {
+  //  add chart js
+
+  return coins.map(coin => (
+    <li key={coin.name}>
+      {coin.name}
+      {coin.rate}
+      {coin.to}
+    </li>
+  ));
+};
+
 const App = () => {
-  async function fetchValue() {
-    const response = await fetch(
-      `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=${API_KEY}`
-    );
-    const data = await response.json();
-    return data;
+  const topCoins = ["btc" /* , "xrp", "eth", "bch", "eos" */];
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [coins, setCoins] = useState([]);
+
+  async function fetchValues(coins) {
+    try {
+      const promises = coins.map(coin =>
+        fetch(
+          `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${coin}&to_currency=USD&apikey=${API_KEY}`
+        )
+      );
+      const responses = await Promise.all(promises);
+      const preJSON = responses.map(response => response.json());
+      const data = await Promise.all(preJSON);
+      return data;
+    } catch (err) {
+      console.log("err");
+      setErrorMessage("Error fetching data. Please try again.");
+    }
   }
 
   useEffect(() => {
-    const data = fetchValue().then(data => console.log(data));
-  });
+    fetchValues(topCoins).then(data => {
+      const coins = data.map(coin => {
+        const { ["Realtime Currency Exchange Rate"]: values } = coin;
+        return {
+          name: values["1. From_Currency Code"],
+          to: values["3. To_Currency Code"],
+          rate: values["5. Exchange Rate"]
+        };
+      });
+      setCoins(coins);
+    });
+  }, []);
 
   const onSubmit = () => {
     // api call
@@ -24,8 +59,9 @@ const App = () => {
   return (
     <div>
       <h1>Market Tracker</h1>
+      {errorMessage && <p>{errorMessage}</p>}
       <Form onSubmit={onSubmit} />
-      {/* add chart js */}
+      <Coins coins={coins} />
     </div>
   );
 };
